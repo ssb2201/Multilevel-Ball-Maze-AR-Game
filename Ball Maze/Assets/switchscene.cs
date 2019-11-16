705adf5235b74e207fd3ea04495150e556993b71
ssb2201 <sarveshbibhuty@gmail.com>
2019-11-16 22:30:14 +0530

    First Commit

---
+++ Ball Maze/Assets/switchscene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class switchscene : MonoBehaviour
+{
+   public int index=0;
+   //public string levelname;
+   
+    void OnTriggerEnter(Collider other)
+    {
+	
+        if (index<3)
+        {
+            	index++;
+		
+		SceneManager.LoadScene(index);
+        }
+
+	
+           // SceneManager.LoadScene(levelname);
+        
+    }
+
+
+   
+}
---
+++ Ball Maze/Assets/timer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float currentTime = 0;
+    float startingTime = 30;
+  
+    [SerializeField] Text countdownText; 
+    
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= Time.deltaTime;
+        // print(currentTime);
+        if (currentTime > 0)
+        {
+
+            countdownText.text = "Timer :" + currentTime.ToString();
+        }
+        else
+        {
+            SceneManager.LoadScene(3);
+        }
+    }
+}
